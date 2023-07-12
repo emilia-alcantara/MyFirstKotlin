572a574cc8706f43687345d72c95ad20c1c33b44
Emilia <correoalcantaraemilia@gmail.com>
2023-07-11 22:28:48 -0400

    Corrección de las formulas!

--- app/src/main/java/cl/individual/myfirstkotlin/Main.kt
+++ app/src/main/java/cl/individual/myfirstkotlin/Main.kt
@@ -9,10 +9,10 @@ fun main() {
     println("Celsius a Fahrenheit: $fahrenheitFromCelsius")
 
    // De Fahrenheit a Kelvin: K = 5/9 (°F - 32) + 273.15
-    val kelvinFromFahren = 5.0/9.0 * (fahrenheitFromCelsius - 32) + 273.15
+    val kelvinFromFahren = 5.0/9.0 * (input - 32) + 273.15
     println("Fahrenheit a Kelvin $kelvinFromFahren")
 
     // Kelvin a Celsius: °C = K - 273.15
-    val celsiusFromKelvin = kelvinFromFahren - 273.15
+    val celsiusFromKelvin = input - 273.15
     println("Kelvin a Celsius: $celsiusFromKelvin")
 }
